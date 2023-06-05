abb9132cb22c5d0c1b765180e57a36ec2e831b2f
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-06 02:08:33 +0600

    Room post and room get api set

--- index.js
+++ index.js
@@ -30,6 +30,42 @@ async function run() {
     const roomsCollection = client.db('aircncDb').collection('rooms')
     const bookingsCollection = client.db('aircncDb').collection('bookings')
 
+
+    //  Save user email and role in db
+    app.put('/users/:email', async(req,res)=>{
+        const email = req.params.email;
+        const user = req.body;
+        const query = {email: email};
+        const options = {upsert: true};
+        const updateDoc = {
+            $set: user
+        };
+        const result = await usersCollection.updateOne(query,updateDoc,options);
+        console.log(result);
+        res.send(result);
+    })
+
+    // save a room in database
+    app.post('/rooms', async(req,res)=>{
+      const room = req.body;
+      const result = await roomsCollection.insertOne(room);
+      res.send(result)
+    })
+
+    // get all rooms
+    app.get('/rooms', async(req,res)=>{
+      const result = await roomsCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
