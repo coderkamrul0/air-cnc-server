c74b4eec1cd29e81f64e66cfefcb66c487814666
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-06 03:58:31 +0600

    Added

--- index.js
+++ index.js
@@ -58,6 +58,14 @@ async function run() {
       res.send(result);
     })
 
+    // get a single room
+    app.get('/room/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    })
+
 
 
 
